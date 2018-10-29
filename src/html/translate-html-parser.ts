8f774b93caec1ff09bbbcc61276262ec2f2cb130
Micha Reiser <micha@famreiser.ch>
2018-10-29 19:40:50 +0100

    Upgrade acorn, cheerio, escodegen and htmlparser2

--- src/html/translate-html-parser.ts
+++ src/html/translate-html-parser.ts
@@ -1,16 +1,16 @@
 import htmlparser = require("htmlparser2");
 
 import Translation from "../translation";
-import ElementContext, {Attributes} from "./element-context";
+import ElementContext, { Attributes } from "./element-context";
 import TranslateLoaderContext from "../translate-loader-context";
-import {matchAngularExpressions, AngularExpressionMatch} from "./ng-filters";
+import { matchAngularExpressions, AngularExpressionMatch } from "./ng-filters";
 
 export const SUPPRESS_ATTRIBUTE_NAME = "suppress-dynamic-translation-error";
 const angularExpressionRegex = /^{{.*}}$/;
 const translateAttributeRegex = /^translate-attr-.*$/;
 
 function isAngularExpression(value: string): boolean {
-    return angularExpressionRegex.test(value);
+  return angularExpressionRegex.test(value);
 }
 
 /**
@@ -20,164 +20,215 @@ function isAngularExpression(value: string): boolean {
  * Attributes translated with the translate filter are handled in the opentag event
  * Expressions used in the body of an element are translated in the text event.
  */
-export  default class TranslateHtmlParser implements htmlparser.Handler {
-    context = new ElementContext(null, "root", null);
-    parser: htmlparser.Parser;
-    html: string;
+export default class TranslateHtmlParser implements htmlparser.Handler {
+  context = new ElementContext(null, "root", null);
+  parser: htmlparser.Parser;
+  html: string;
 
-    constructor(private loader: TranslateLoaderContext) {
-        this.ontext = this.ontext.bind(this);
+  constructor(private loader: TranslateLoaderContext) {
+    this.ontext = this.ontext.bind(this);
+  }
+
+  parse(html: string): void {
+    this.html = html;
+    this.parser = new htmlparser.Parser(this, { decodeEntities: true });
+    this.parser.parseComplete(html);
+
+    this.html = this.parser = null;
+  }
+
+  onopentag(name: string, attributes: Attributes): void {
+    this.context = this.context.enter(name, attributes);
+
+    if (name === "translate") {
+      this.context.translateDirective = true;
+    } else if (typeof attributes["translate"] !== "undefined") {
+      this.context.translateDirective = true;
+      this.context.translationId = attributes["translate"];
     }
 
-    parse(html: string): void {
-        this.html = html;
-        this.parser = new htmlparser.Parser(this, { decodeEntities: true });
-        this.parser.parseComplete(html);
+    this.context.suppressDynamicTranslationErrors =
+      typeof attributes[SUPPRESS_ATTRIBUTE_NAME] !== "undefined";
 
-        this.html = this.parser = null;
+    // <any attr='{{ x | translate }}'></any>
+    Object.keys(attributes).forEach(name =>
+      this.handleAngularExpression(attributes[name])
+    );
+
+    if (!this.context.translateDirective) {
+      // should not be translated
+      return;
     }
 
-    onopentag(name: string, attributes: Attributes): void {
-        this.context = this.context.enter(name, attributes);
+    this.context.elementStartPosition = getStartIndex(this.parser);
 
-        if (name === "translate") {
-            this.context.translateDirective = true;
-        } else if (typeof(attributes["translate"]) !== "undefined") {
-            this.context.translateDirective = true;
-            this.context.translationId = attributes["translate"];
-        }
+    // translate-attr-*
+    const translateAttributes = Object.keys(attributes).filter(key =>
+      translateAttributeRegex.test(key)
+    );
+    this.context.translateAttributes = translateAttributes.length > 0;
 
-        this.context.suppressDynamicTranslationErrors = typeof(attributes[SUPPRESS_ATTRIBUTE_NAME]) !== "undefined";
+    // If an attribute is translated, then the content will not be translated.
+    // Extracts the translations where translate-attr-ATTR defines the id and translate-default-ATTR the default text
+    translateAttributes.forEach(attributeName => {
+      var translationId = attributes[attributeName],
+        translatedAttributeName = attributeName.substr(
+          "translate-attr-".length
+        ),
+        defaultText =
+          attributes["translate-default-attr-" + translatedAttributeName];
 
-        // <any attr='{{ x | translate }}'></any>
-        Object.keys(attributes).forEach(name => this.handleAngularExpression(attributes[name]));
+      this.registerTranslation(translationId, defaultText);
+    });
 
-        if (!this.context.translateDirective) {
-            // should not be translated
-            return;
-        }
+    this.context.defaultText = attributes["translate-default"];
+  }
 
-        this.context.elementStartPosition = this.parser.startIndex;
+  private handleAngularExpression(value: string): void {
+    var matches = matchAngularExpressions(value);
+    matches.forEach(this.handleFilterMatch, this);
 
-        // translate-attr-*
-        const translateAttributes = Object.keys(attributes).filter(key => translateAttributeRegex.test(key));
-        this.context.translateAttributes = translateAttributes.length > 0;
+    if (matches.length > 0) {
+      let translationId = matches[0].value;
 
-        // If an attribute is translated, then the content will not be translated.
-        // Extracts the translations where translate-attr-ATTR defines the id and translate-default-ATTR the default text
-        translateAttributes.forEach(attributeName => {
-            var translationId = attributes[attributeName],
-                translatedAttributeName = attributeName.substr("translate-attr-".length),
-                defaultText = attributes["translate-default-attr-" + translatedAttributeName];
+      if (/^".*"$/.test(translationId) || /^.*'$/.test(translationId)) {
+        translationId = translationId.substring(1, translationId.length - 1);
+        this.registerTranslation(translationId);
+      }
+    }
+  }
 
-            this.registerTranslation(translationId, defaultText);
-        });
+  ontext(text: string): void {
+    this.context.text = text = text.trim();
 
-        this.context.defaultText = attributes["translate-default"];
+    // Content of an element that is translated
+    if (this.context.translateDirective) {
+      // The translation will be registered with the close tag.
+      this.context.translationId = this.context.translationId || text;
+    } else {
+      // only attributes are translated or no translation at all
+      var expressionMatches = matchAngularExpressions(text);
+      expressionMatches.forEach(this.handleFilterMatch, this);
+    }
+  }
+
+  private handleFilterMatch(match: AngularExpressionMatch): void {
+    var translationId = match.value;
+
+    if (!(/^".*"$/.test(translationId) || /^.*'$/.test(translationId))) {
+      this.emitSuppressableError(
+        `A dynamic filter expression is used in the text or an attribute of the element '${this.context.asHtml()}'. Add the '${SUPPRESS_ATTRIBUTE_NAME}' attribute to suppress the error (ensure that you have registered the translation manually, consider using i18n.registerTranslation).`
+      );
+    } else if (match.previousFilters) {
+      this.emitSuppressableError(
+        `Another filter is used before the translate filter in the element ${this.context.asHtml()}. Add the '${SUPPRESS_ATTRIBUTE_NAME}' to suppress the error (ensure that you have registered the translation manually, consider using i18n.registerTranslation).`
+      );
+    } else {
+      translationId = translationId.substring(1, translationId.length - 1);
+      this.registerTranslation(translationId);
+    }
+  }
+
+  onclosetag(name: string): void {
+    if (this.context.elementName !== name) {
+      this.emitSuppressableError(
+        "Error parsing html, close tag does not match open tag"
+      );
     }
 
-    private handleAngularExpression(value: string): void {
-        var matches = matchAngularExpressions(value);
-        matches.forEach(this.handleFilterMatch, this);
-
-        if (matches.length > 0) {
-            let translationId = matches[0].value;
-
-            if (/^".*"$/.test(translationId) || /^.*'$/.test(translationId)) {
-                translationId = translationId.substring(1, translationId.length - 1);
-                this.registerTranslation(translationId);
-            }
-        }
+    // <any translate='test'></any>
+    if (this.context.translateDirective) {
+      if (this.context.translationId) {
+        this.registerTranslation(
+          this.context.translationId,
+          this.context.defaultText,
+          this.context.elementStartPosition
+        );
+      } else if (!this.context.translateAttributes) {
+        // no translate-attr-* and the element has not specified a translation id, someone is using the directive incorrectly
+        this.emitSuppressableError(
+          "the element uses the translate directive but does not specify a translation id nor has any translated attributes (translate-attr-*). Specify a translation id or remove the translate-directive."
+        );
+      }
     }
 
-    ontext(text: string): void {
-        this.context.text = text = text.trim();
+    this.context = this.context.leave();
+  }
 
-        // Content of an element that is translated
-        if (this.context.translateDirective) {
-            // The translation will be registered with the close tag.
-            this.context.translationId = this.context.translationId || text;
-        } else {
-            // only attributes are translated or no translation at all
-            var expressionMatches = matchAngularExpressions(text);
-            expressionMatches.forEach(this.handleFilterMatch, this);
-        }
+  onerror(error: Error): void {
+    this.emitError(
+      `Failed to parse the html, error is ${error.message}`,
+      this.loc()
+    );
+  }
+
+  private emitSuppressableError(
+    message: string,
+    loc: { line: number; column: number } = this.loc()
+  ): void {
+    if (this.context.suppressDynamicTranslationErrors) {
+      return;
     }
 
-    private handleFilterMatch(match: AngularExpressionMatch): void {
-        var translationId = match.value;
+    this.emitError(message, loc);
+  }
 
-        if (!(/^".*"$/.test(translationId) || /^.*'$/.test(translationId))) {
-            this.emitSuppressableError(`A dynamic filter expression is used in the text or an attribute of the element '${this.context.asHtml()}'. Add the '${SUPPRESS_ATTRIBUTE_NAME}' attribute to suppress the error (ensure that you have registered the translation manually, consider using i18n.registerTranslation).`);
-        } else if (match.previousFilters) {
-            this.emitSuppressableError(`Another filter is used before the translate filter in the element ${this.context.asHtml()}. Add the '${SUPPRESS_ATTRIBUTE_NAME}' to suppress the error (ensure that you have registered the translation manually, consider using i18n.registerTranslation).`);
-        } else {
-            translationId = translationId.substring(1, translationId.length - 1);
-            this.registerTranslation(translationId);
-        }
+  private emitError(
+    message: string,
+    loc: { line: number; column: number }
+  ): void {
+    message = `Failed to extract the angular-translate translations from ${
+      this.loader.resource
+    }:${loc.line}:${loc.column}: ${message}`;
+
+    this.loader.emitError(message);
+  }
+
+  private registerTranslation(
+    translationId: string,
+    defaultText?: string,
+    position?: number
+  ): void {
+    const loc = this.loc(position);
+    if (
+      isAngularExpression(translationId) ||
+      isAngularExpression(defaultText)
+    ) {
+      this.emitSuppressableError(
+        `The element '${this.context.asHtml()}'  in '${
+          this.loader.resource
+        }' uses an angular expression as translation id ('${translationId}') or as default text ('${defaultText}'), this is not supported. To suppress this error at the '${SUPPRESS_ATTRIBUTE_NAME}' attribute to the element or any of its parents.`,
+        loc
+      );
+      return;
     }
 
-    onclosetag(name: string): void {
-        if (this.context.elementName !== name) {
-            this.emitSuppressableError("Error parsing html, close tag does not match open tag");
-        }
+    this.loader.registerTranslation(
+      new Translation(translationId, defaultText, {
+        resource: this.loader.resource,
+        loc: loc
+      })
+    );
+  }
 
-        // <any translate='test'></any>
-        if (this.context.translateDirective) {
-            if (this.context.translationId) {
-                this.registerTranslation(this.context.translationId, this.context.defaultText, this.context.elementStartPosition);
-            } else if (!this.context.translateAttributes) {
-                // no translate-attr-* and the element has not specified a translation id, someone is using the directive incorrectly
-                this.emitSuppressableError("the element uses the translate directive but does not specify a translation id nor has any translated attributes (translate-attr-*). Specify a translation id or remove the translate-directive.");
-            }
-        }
-
-        this.context = this.context.leave();
+  private loc(
+    position: number = getStartIndex(this.parser)
+  ): { line: number; column: number } {
+    let line = 1;
+    let column = 0;
+    for (let i = 0; i < position; ++i) {
+      if (this.html[i] === "\n") {
+        ++line;
+        column = 0;
+      } else {
+        ++column;
+      }
     }
 
-    onerror(error: Error): void {
-        this.emitError(`Failed to parse the html, error is ${error.message}`, this.loc());
-    }
+    return { line: line, column: column };
+  }
+}
 
-    private emitSuppressableError(message: string, loc: { line: number, column: number } = this.loc()): void {
-        if (this.context.suppressDynamicTranslationErrors) {
-            return;
-        }
-
-        this.emitError(message, loc);
-    }
-
-    private emitError(message: string, loc: { line: number, column: number }): void {
-        message = `Failed to extract the angular-translate translations from ${this.loader.resource}:${loc.line}:${loc.column}: ${message}`;
-
-        this.loader.emitError(message);
-    }
-
-    private registerTranslation(translationId: string, defaultText?: string, position?: number): void {
-        const loc = this.loc(position);
-        if (isAngularExpression(translationId) || isAngularExpression(defaultText)) {
-            this.emitSuppressableError(`The element '${this.context.asHtml()}'  in '${this.loader.resource}' uses an angular expression as translation id ('${translationId}') or as default text ('${defaultText}'), this is not supported. To suppress this error at the '${SUPPRESS_ATTRIBUTE_NAME}' attribute to the element or any of its parents.`, loc);
-            return;
-        }
-
-        this.loader.registerTranslation(new Translation(translationId, defaultText, {
-            resource: this.loader.resource,
-            loc: loc
-        }));
-    }
-
-    private loc(position: number = this.parser.startIndex): { line: number, column: number } {
-        let line = 1;
-        let column = 0;
-        for (let i = 0; i < position; ++i) {
-            if (this.html[i] === "\n") {
-                ++line;
-                column = 0;
-            } else {
-                ++column;
-            }
-        }
-
-        return { line: line, column: column };
-    }
+function getStartIndex(parser: htmlparser.Parser): number {
+  return (parser as any).startIndex;
 }
--- src/js/js-loader.ts
+++ src/js/js-loader.ts
@@ -4,16 +4,16 @@ import sourceMap = require("source-map");
 import TranslateLoaderContext from "../translate-loader-context";
 import TranslateVisitor from "./translate-visitor";
 import CodeWithSourceMap = SourceMap.CodeWithSourceMap;
-import *  as loaderUtils from "loader-utils";
+import * as loaderUtils from "loader-utils";
 
 /**
  * The optional options passed to the plugin
  */
 interface LoaderOptions {
-    /**
-     * Optional acorn options that are passed to the parser
-     */
-    parserOptions?: acorn.Options;
+  /**
+   * Optional acorn options that are passed to the parser
+   */
+  parserOptions?: acorn.Options;
 }
 
 /**
@@ -26,61 +26,76 @@ interface LoaderOptions {
  * @param sourceMaps
  */
 function jsLoader(source: string, sourceMaps: any): void {
-    const loader: TranslateLoaderContext = this;
-    if (!loader.registerTranslation) {
-        return this.callback(new Error("The WebpackAngularTranslate plugin is missing. Add the plugin to your webpack configurations 'plugins' section."), source, sourceMaps);
-    }
+  const loader: TranslateLoaderContext = this;
+  if (!loader.registerTranslation) {
+    return this.callback(
+      new Error(
+        "The WebpackAngularTranslate plugin is missing. Add the plugin to your webpack configurations 'plugins' section."
+      ),
+      source,
+      sourceMaps
+    );
+  }
 
-    if (loader.cacheable) {
-        loader.cacheable();
-    }
+  if (loader.cacheable) {
+    loader.cacheable();
+  }
 
-    if (isExcludedResource(loader.resourcePath)) {
-        return this.callback(null, source, sourceMaps);
-    }
+  if (isExcludedResource(loader.resourcePath)) {
+    return this.callback(null, source, sourceMaps);
+  }
 
-    extractTranslations(loader, source, sourceMaps);
+  extractTranslations(loader, source, sourceMaps);
 }
 
-function extractTranslations(loader: TranslateLoaderContext, source: string, sourceMaps: any): void {
-    const options = loaderUtils.getOptions<LoaderOptions>(loader) || {};
-    const parserOptions = options.parserOptions || {};
+function extractTranslations(
+  loader: TranslateLoaderContext,
+  source: string,
+  sourceMaps: any
+): void {
+  const options = loaderUtils.getOptions<LoaderOptions>(loader) || {};
+  const parserOptions = options.parserOptions || {};
 
-    loader.pruneTranslations(loader.resource);
+  loader.pruneTranslations(loader.resource);
 
-    const visitor = new TranslateVisitor(loader, parserOptions);
-    let ast: ESTree.Node = acorn.parse(source, visitor.options);
-    ast = visitor.visit(ast);
+  const visitor = new TranslateVisitor(loader, parserOptions);
+  let ast: ESTree.Node = acorn.parse(source, visitor.options);
+  ast = visitor.visit(ast);
 
-    let code = source;
+  let code = source;
 
-    if (visitor.changedAst) {
-        const generateSourceMaps = !!(loader.sourceMap || sourceMaps);
-        const result = escodegen.generate(ast, {
-            comment: true,
-            sourceMap: generateSourceMaps ? loader.resourcePath : undefined,
-            sourceMapWithCode: generateSourceMaps,
-            sourceContent: generateSourceMaps ? source : undefined
-        });
+  if (visitor.changedAst) {
+    const generateSourceMaps = !!(loader.sourceMap || sourceMaps);
+    const result = escodegen.generate(ast, {
+      comment: true,
+      sourceMap: generateSourceMaps ? loader.resourcePath : undefined,
+      sourceMapWithCode: generateSourceMaps,
+      sourceContent: generateSourceMaps ? source : undefined
+    });
 
-        if (generateSourceMaps) {
-            const codeWithSourceMap = <CodeWithSourceMap> result;
-            code = codeWithSourceMap.code;
-            if (sourceMaps) {
-                // Create a new source maps that is a mapping from original Source -> result from previous loader -> result from this loader
-                var originalSourceMap = new sourceMap.SourceMapConsumer(sourceMaps);
-                codeWithSourceMap.map.applySourceMap(originalSourceMap, loader.resourcePath);
-            }
+    if (generateSourceMaps) {
+      const codeWithSourceMap = <CodeWithSourceMap>(result as any);
+      code = codeWithSourceMap.code;
+      if (sourceMaps) {
+        // Create a new source maps that is a mapping from original Source -> result from previous loader -> result from this loader
+        var originalSourceMap = new sourceMap.SourceMapConsumer(sourceMaps);
+        codeWithSourceMap.map.applySourceMap(
+          originalSourceMap,
+          loader.resourcePath
+        );
+      }
 
-            sourceMaps = (<any>codeWithSourceMap.map).toJSON();
-        }
+      sourceMaps = (<any>codeWithSourceMap.map).toJSON();
     }
+  }
 
-    loader.callback(null, code, sourceMaps);
+  loader.callback(null, code, sourceMaps);
 }
 
 function isExcludedResource(resource: string): boolean {
-    return /angular-translate[\/\\]dist[\/\\]angular-translate\.js$/.test(resource);
+  return /angular-translate[\/\\]dist[\/\\]angular-translate\.js$/.test(
+    resource
+  );
 }
 
 export = jsLoader;
