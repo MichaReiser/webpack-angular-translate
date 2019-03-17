65ba6b47bb1b687354d63d29de8993f0a01ec3a1
Micha Reiser <micha@famreiser.ch>
2019-03-17 17:38:01 +0000

    Migrate translation.spec to Jest

--- test/translation.spec.js
+++ test/translation.spec.js
@@ -1,5 +1,4 @@
-var assert = require("chai").assert,
-  Translation = require("../dist/translation").Translation;
+var Translation = require("../dist/translation").Translation;
 
 describe("Translations", function() {
   "use strict";
@@ -7,7 +6,7 @@ describe("Translations", function() {
   it("creates a new translation with a resource array if a single resource is passed in", function() {
     var translation = new Translation("test", null, "src/main.html");
 
-    assert.sameMembers(translation.usages, ["src/main.html"]);
+    expect(translation.usages).toEqual(["src/main.html"]);
   });
 
   it("creates a new translation with a resource array, if a resource array is passed in", function() {
@@ -16,7 +15,7 @@ describe("Translations", function() {
       "src/login.html"
     ]);
 
-    assert.sameMembers(translation.usages, ["src/main.html", "src/login.html"]);
+    expect(translation.usages).toEqual(["src/main.html", "src/login.html"]);
   });
 
   it("merges the resources when two translations are merged", function() {
@@ -24,9 +23,9 @@ describe("Translations", function() {
       second = new Translation("hallo", null, "src/login.html");
 
     var merged = first.merge(second);
-    assert.sameMembers(merged.usages, ["src/main.html", "src/login.html"]);
-    assert.equal(merged.id, "hallo");
-    assert.isNull(merged.defaultText);
+    expect(merged.usages).toEqual(["src/main.html", "src/login.html"]);
+    expect(merged.id).toBe("hallo");
+    expect(merged.defaultText).toBeNull();
   });
 
   it("does not include the same resources twice when merging", function() {
@@ -34,9 +33,9 @@ describe("Translations", function() {
       second = new Translation("hallo", null, "src/main.html");
 
     var merged = first.merge(second);
-    assert.sameMembers(merged.usages, ["src/main.html"]);
-    assert.equal(merged.id, "hallo");
-    assert.isNull(merged.defaultText);
+    expect(merged.usages).toEqual(["src/main.html"]);
+    expect(merged.id).toBe("hallo");
+    expect(merged.defaultText).toBeNull();
   });
 
   it("uses the default text of the translation if one of both translations have a default text", function() {
@@ -44,8 +43,8 @@ describe("Translations", function() {
       second = new Translation("hallo", "Hallo", "src/main.html");
 
     var merged = first.merge(second);
-    assert.equal(merged.id, "hallo");
-    assert.equal(merged.defaultText, "Hallo");
+    expect(merged.id).toBe("hallo");
+    expect(merged.defaultText).toBe("Hallo");
   });
 
   it("uses the default text of the first translations if both translations have a default text", function() {
@@ -53,8 +52,8 @@ describe("Translations", function() {
       second = new Translation("hallo", "Hallo", "src/main.html");
 
     var merged = first.merge(second);
-    assert.equal(merged.id, "hallo");
-    assert.equal(merged.defaultText, "Hello");
+    expect(merged.id).toBe("hallo");
+    expect(merged.defaultText).toBe("Hello");
   });
 
   it("implements to string", function() {
@@ -63,8 +62,7 @@ describe("Translations", function() {
       { resource: "src/login.html", loc: undefined }
     ]);
 
-    assert.equal(
-      translation.toString(),
+    expect(translation.toString()).toBe(
       "{ id: 'hallo', defaultText: 'null', usages: [ src/main.html:10:4, src/login.html:null:null ] }"
     );
   });
@@ -72,18 +70,18 @@ describe("Translations", function() {
   it("text returns the translation id if the translation has no default text", function() {
     var translation = new Translation("Hello");
 
-    assert.equal(translation.text, "Hello");
+    expect(translation.text).toBe("Hello");
   });
 
   it("text returns the default text if the translation has a default text", function() {
     var translation = new Translation("Hello", "Hallo");
 
-    assert.equal(translation.text, "Hallo");
+    expect(translation.text).toBe("Hallo");
   });
 
   it("text returns a string, if a non string value is set as translation id or default text", function() {
     var translation = new Translation("number", 5);
 
-    assert.equal(translation.text, "5");
+    expect(translation.text).toBe("5");
   });
 });
